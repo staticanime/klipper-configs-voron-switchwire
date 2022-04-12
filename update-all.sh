17fd47bce365f88b7812a3cf4c6c9b8cea85493a
staticanime <static@staticanime.net>
2022-04-12 19:50:26 +0100

    Scripted auto-commit on change (2022-04-12 19:50:26) by gitwatch.sh

---
+++ update-all.sh
@@ -0,0 +1,20 @@
+sudo service klipper stop
+cd ~/klipper
+git pull
+
+make clean KCONFIG_CONFIG=config.skr
+make menuconfig KCONFIG_CONFIG=config.skr
+make KCONFIG_CONFIG=config.skr
+read -p "SKR Mini E3 V2.0 firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+
+./scripts/flash-sdcard.sh /dev/ttyACM0 btt-skr-mini-e3-v2
+read -p "SKR Mini E3 V2.0 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
+
+make clean KCONFIG_CONFIG=config.rpi
+make menuconfig KCONFIG_CONFIG=config.rpi
+
+make KCONFIG_CONFIG=config.rpi
+read -p "RPi firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+make flash KCONFIG_CONFIG=config.rpi
+
+sudo service klipper start
